29bbb4f7d7a23e0e3174d830be871b351bc70f27
uuuu1313 <a51903153@gmail.com>
2024-05-30 00:07:43 +0900

    URLs와 Views

---
+++ mysite/polls/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Polls's index#")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
